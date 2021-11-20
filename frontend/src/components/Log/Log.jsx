231e7d06f4394c3433e6dea88669122aada59e2a
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-11-20 10:44:28 +0100

    Affichage formulaire selon SignUp/Login

--- frontend/src/components/Log/Log.jsx
+++ frontend/src/components/Log/Log.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 
 const Log = () => {
+
+    const [signUp, setSignUp] = useState(true);
+    const [login, setLogin] = useState(false);
+
+    const handleForm = (e) => {
+        if(e.target.id === "register") {
+            setLogin(false)
+            setSignUp(true)
+        } else if(e.target.id === 'login') {
+            setSignUp(false)
+            setLogin(true)
+        }
+    }
+
     return (
         <div>
-            <SignUpForm/>
+            <div className="form_container">
+                <ul>
+                    <li onClick={handleForm} id='register'>S'inscrire</li>
+                    <li onClick={handleForm} id='login'>Se connecter</li>
+                </ul>
+                {signUp && <SignUpForm />}
+                {login && <LoginForm />}
+            </div>
         </div>
     );
 };
---
+++ frontend/src/components/Log/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return (
+        <div>
+            <h2>Je suis le Login Formulaire !</h2>
+        </div>
+    );
+};
+
+export default LoginForm;
