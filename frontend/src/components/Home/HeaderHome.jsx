ca76d0a8436716f1a9dc047ce0c02ab4ed2922f5
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2021-12-01 15:41:47 +0100

    Test Logout OK

--- frontend/src/components/Home/HeaderHome.jsx
+++ frontend/src/components/Home/HeaderHome.jsx
@@ -1,12 +1,21 @@
+import axios from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const HeaderHome = () => {
+
+    const logout = async (e) => {
+        await axios({
+            method: 'GET',
+            url: 'http://localhost:5000/api/user/logout',
+        })
+    }
+
     return (
         <div className='header_container'>
             <h3 className='title_header'>User Name</h3>
             <Link to='/profil/{user.id}' className='link_header'><i className="far fa-id-card profil"></i></Link>
-            <Link to='/' className='link_header'><i className="fas fa-sign-out-alt signout"></i></Link>
+            <Link to='/' className='link_header' onClick={logout}><i className="fas fa-sign-out-alt signout"></i></Link>
         </div>
     );
 };
